818116ccf21240948aded0fdfafe3fc7401d1fe6
Devendra Dewangan <devendra@smilebots.com>
2019-06-29 13:43:59 +0530

    2nd

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCYxmrSvd4cxWKAZ9ZmSRAhWaYFoyafIHM",
+  authDomain: "themad-b31c4.firebaseapp.com",
+  databaseURL: "https://themad-b31c4.firebaseio.com",
+  projectId: "themad-b31c4",
+  storageBucket: "themad-b31c4.appspot.com",
+  messagingSenderId: "822348548496",
+  appId: "1:822348548496:web:25f5b92ddc80e861"
+};
