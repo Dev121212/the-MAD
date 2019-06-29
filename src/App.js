d9a2650b99241f02109601d535ed810b4b82d3b5
Mohit23x <mohit@smilebots.com>
2019-06-29 14:29:57 +0530

    second comit

--- src/App.js
+++ src/App.js
@@ -2,26 +2,8 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./containers/Home";
-import { auth } from "./firebase";
 
 class App extends React.Component {
-  signInAnonymous = () => {
-    auth
-      .signInAnonymously()
-      .then(usr => {
-        console.log("usr");
-        console.log(usr.uid);
-      })
-      .catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-        console.log(errorCode);
-        console.log(errorMessage);
-      });
-  };
-
   componentDidMount() {}
 
   render() {
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,14 +1,121 @@
 import React from "react";
 import DashboardContainer from "../containers/Dashboard";
+import { auth, db } from "../firebase";
+import firebase from '../firebase';
 
-class DashboardContainer extends React.Component {
+
+class Home extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+       isAnonymous: false,
+       uid: '',
+       clickCount: 0,
+      showGoogleButton: false
+    }
+  }
+  
+  signInAnonymous = () => {
+    let dis = this;
+    console.log('called', this.state.clickCount);
+    dis.setState({
+      clickCount: dis.state.clickCount + 1
+    })
+
+
+    if(this.state.clickCount < 2){
+      auth.onAuthStateChanged(function (user) {
+        if (user) {
+          console.log('logged in');
+          var isAnonymous = user.isAnonymous;
+          var uid = user.uid;
+          dis.setState({
+            isAnonymous: isAnonymous,
+            uid: uid
+          })
+          db.collection('users').doc(uid).set({
+            from: 'anonymous'
+          })
+        } else {
+          console.log('not logged in');
+          auth
+            .signInAnonymously()
+            .then(usr => {
+              console.log("usr");
+              console.log(usr.uid);
+            })
+            .catch(function (error) {
+              // Handle Errors here.
+              var errorCode = error.code;
+              var errorMessage = error.message;
+              // ...
+              console.log(errorCode);
+              console.log(errorMessage);
+            });
+        }
+      });
+    }else{
+      this.setState({
+        showGoogleButton: true
+      })
+    }
+
+  };
+
+  handleGoogleLogin = () => {
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then(async function (result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+
+      var credential = await firebase.auth.GoogleAuthProvider.credential(result.getAuthResponse().id_token);
+
+      auth.currentUser.linkAndRetrieveDataWithCredential(credential).then(function (usercred) {
+        var user = usercred.user;
+        console.log("Anonymous account successfully upgraded", user);
+      }, function (error) {
+        console.log("Error upgrading anonymous account", error);
+      });
+
+      db.collection('users').doc(user.uid).set({
+        from: 'google'
+      })
+
+      // ...
+    }).catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+
+  }
+
   render() {
     return (
       <div>
-        <DashboardContainer />
+        <button onClick={this.signInAnonymous} disabled={this.state.showGoogleButton}>
+          Start the Game
+        </button>
+        {/* <DashboardContainer /> */}
+        <h1>
+          is {this.state.isAnonymous}
+        </h1>
+        <h1>
+          {this.state.uid}
+        </h1>
+        {this.state.showGoogleButton ? <button onClick={this.handleGoogleLogin}>Log in with Google</button> : null}
       </div>
     );
   }
 }
 
-export default DashboardContainer;
+export default Home;
